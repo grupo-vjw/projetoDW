c191a837ddeb304fed82e8ab896e346d07bbdee5
joseamarildo <joseamaril@gmail.com>
2013-04-02 15:15:17 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Cadastro</title>
+</head>
+<body>
+
+<form method="post" action="insere_cadastro.php">
+<label> Nome do usuário </label>
+<input type="text" name="nome" maxlength="50" />
+
+<label> Usuário </label>
+<input type="text" name="usuario" maxlength="50" />
+
+<label> Senha </label>
+<input type="password" name="senha" maxlength="50" />
+
+<input type="submit" value="Entrar" />
+</form> 
+
+</body>
+</html>
