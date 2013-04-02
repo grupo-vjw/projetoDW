9f7c8d99f8adeb8578eccac43b31728a7ffbb088
joseamarildo <joseamaril@gmail.com>
2013-04-02 15:09:40 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>login</title>
+</head>
+<body>
+
+<form method="post" action="seguranca.php">
+<label> Usuário </label>
+<input type="text" name="usuario" maxlength="50" />
+
+<label> Senha </label>
+<input type="password" name="senha" maxlength="50" />
+
+<input type="submit" value="Entrar" />
+</form>
+
+</body>
+</html>
